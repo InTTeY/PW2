91ada0ca1dffef19fb3dd78d99aa58f5bb47cb62
InTTeY <123205539+InTTeY@users.noreply.github.com>
2023-03-20 02:24:29 +0300

    Add files via upload

---
+++ PW2_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace PW2_1
+{
+    class SquareEquation
+    {
+        public double a;
+        public double b;
+        public double c;
+        private double D;
+        private double x1;
+        private double x2;
+
+        public void Calculations()
+        {
+            Discriminant();
+            CalculateRoots();
+        }
+        private void Discriminant()
+        {
+            D = Math.Pow(b, 2) - 4 * a * c;
+        }
+        private void CalculateRoots()
+        {
+            if (D < 0)
+            {
+                Console.WriteLine("Корней нет, так как дискриминант меньше 0");
+            }
+            else
+            {
+                if (D == 0)
+                {
+                    double x = -b / (2 * a);
+                    Console.WriteLine("Так как дискриминант равен 0, то есть 1 корень - {0}", x);
+                }
+                else
+                {
+                    x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                    Console.WriteLine("Имеется 2 корня:\nПервый корень - {0}\nВторой корень - {1}", x1, x2);
+                }
+            }
+        }
+    }
+    class program
+    {
+        static void Main()
+        {
+            Console.Write("Введите данные для решения уравнения типа ax^2+bx+c=0\n");
+            SquareEquation squareEquation = new SquareEquation();
+            Console.Write("Введите значение: a = ");
+            squareEquation.a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение: b = ");
+            squareEquation.b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение: c = ");
+            squareEquation.c = Convert.ToDouble(Console.ReadLine());
+            squareEquation.Calculations();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PW2_3/Program.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PW2_3
+{
+    class CesarChifr
+    {
+        public char[] message;
+        public int key;
+        private char[] abc = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+
+        public string Encrypt()
+        {
+            int number_symbol;
+            int bias;
+            string result;
+            for (int i = 0; i < message.Length; i++)
+            {
+                int j;
+                for (j = 0; j < abc.Length; j++)
+                {
+                    if (message[i] == abc[j])
+                    {
+                        break;
+                    }
+                }
+                if (j < 33)
+                {
+                    number_symbol = j;
+                    bias = number_symbol + key;
+                    if (bias > 32)
+                    {
+                        bias = bias - 33;
+                    }
+                    message[i] = abc[bias];
+                }
+            }
+            result = new string(message);
+            return result;
+        }
+        public string Decipher()
+        {
+            int number_symbol;
+            int bias;
+            string result;
+            for (int i = 0; i < message.Length; i++)
+            {
+                int j;
+                for (j = 0; j < abc.Length; j++)
+                {
+                    if (message[i] == abc[j])
+                    {
+                        break;
+                    }
+                }
+                if (j < 33)
+                {
+                    number_symbol = j;
+                    bias = number_symbol + (33 - key);
+
+                    if (bias > 32)
+                    {
+                        bias = bias - 33;
+                    }
+                    message[i] = abc[bias];
+                }
+            }
+            result = new string(message);
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Чтобы выполнить шифрование введите: для шифрования - (ш) или Encrypt( e ), для расшифрования - (р) или Decipher( d ): ");
+            string answer = Convert.ToString(Console.ReadLine());
+            CesarChifr cesar = new CesarChifr();
+            if (answer == "ш" || answer == "Ш" || answer == "e" || answer == "E" || answer == "Encrypt" || answer == "encrypt")
+            {
+                Console.WriteLine("!!!Шифр производится только в нижнем регистре(автоматический перевод) на русском языке!!!");
+                Console.Write("Введите строку для шифрования: ");
+                string m = Convert.ToString(Console.ReadLine());
+                m = m.ToLower();
+                char[] message = m.ToCharArray();
+                cesar.message = message;
+                Console.Write("Введите ключ: ");
+                cesar.key = Convert.ToInt32(Console.ReadLine());
+                string result = cesar.Encrypt();
+                Console.WriteLine("Зашифрованная строка: " + result);
+            }
+            else
+            {
+                if (answer == "р" || answer == "Р" || answer == "d" || answer == "D" || answer == "Decipher" || answer == "decipher")
+                {
+                    Console.WriteLine("!!!Дешифрация производится только в нижнем регистре(автоматический перевод)!!!");
+                    Console.Write("Введите строку для расшифрования: ");
+                    string m = Convert.ToString(Console.ReadLine());
+                    m = m.ToLower();
+                    char[] message = m.ToCharArray();
+                    cesar.message = message;
+                    Console.Write("Введите ключ: ");
+                    cesar.key = Convert.ToInt32(Console.ReadLine());
+                    string result = cesar.Decipher();
+                    Console.WriteLine("Расшифрованная строка: " + result);
+                }
+                else
+                {
+                    Console.WriteLine("Вы не выбрали действие. Производится заверщение программы");
+                }
+            }
+        }
+    }
+}
